803a3e05dc93dd9603dd574b0c815dac9c6be859
tln1728 <tunglamnoibai@gmail.com>
2025-05-11 13:42:48 +0700

    Tiny update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test', function() {
+    $user = App\Models\User::first();
+    dd($user->roles()->toRawSql());
+    dd($user->roles()->get()->toArray());
+});
