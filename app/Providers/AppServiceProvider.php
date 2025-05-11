1ec07c2c3320ecf14777906f89970a01da307fc2
tln1728 <tunglamnoibai@gmail.com>
2025-05-12 01:08:12 +0700

    Publish language file, test register new color

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Support\Colors\Color;
+use Filament\Support\Facades\FilamentColor;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        FilamentColor::register([
+            'tomato' => Color::rgb('rgb(255, 99, 71)'),
+            'tailwind' => [
+                50 => '254, 242, 242',
+                100 => '254, 226, 226',
+                200 => '254, 202, 202',
+                300 => '252, 165, 165',
+                400 => '248, 113, 113',
+                500 => '239, 68, 68',
+                600 => '220, 38, 38',
+                700 => '185, 28, 28',
+                800 => '153, 27, 27',
+                900 => '127, 29, 29',
+                950 => '69, 10, 10',
+            ],
+        ]);
     }
 }
---
+++ lang/vi/validation.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'email'      => 'Email không hợp lệ',
+    'unique'     => ':Attribute này đã tồn tại',
+    'regex'      => ':Attribute không đúng định dạng',
+    'required'   => 'Trường :attribute là bắt buộc',
+    'confirmed'  => ':Attribute xác nhận không khớp', 
+    'same'       => ':Attribute không khớp',
+    // 'same'    => 'Trường :attribute phải khớp với trường :other.',
+
+    // Doesn't work ?
+    // 'attributes' => [
+    //     'password' => 'Mật khẩu',
+    //     'phone' => 'Số điện thoại',
+    //     'email' => 'Email',
+    // ],
+];
